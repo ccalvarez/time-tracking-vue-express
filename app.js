b51f5d693ca64ac7f3e908dfb0b652653bcb68b6
Carolina Cordero <ccalvarez@gmail.com>
2017-06-14 14:50:11 -0600

    Added dotenv package to manage environment variables

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+require('dotenv').config({ path: 'variables.env' });
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = new express();
 
-const connString = 'mongodb://localhost:27017/timetracking';
-
-mongoose.connect(connString);
+mongoose.connect(process.env.DATABASE);
 
 mongoose.connection.on('connected', () => {
-  console.log('Mongoose connection open to ' + connString);
+  console.log('Mongoose connection open to ' + process.env.DATABASE);
   app.listen(3001, 'localhost', () => {
     console.log('Express listening on port 3001!');
   });
